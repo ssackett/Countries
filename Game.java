7367a8d9d6637c3667dbeacb47843a3ae5a5f9ec
Stephen Sackett <sesackett@gmail.com>
2017-07-27 12:17:54 -0400

    check the answer ignore case

--- Game.java
+++ Game.java
@@ -5,7 +5,7 @@ class Game {
 		Console console = System.console();
 		String answer = console.readLine("What is the capital of Australia? ");
 		String capital = "Canberra";
-		if (answer.equals(capital)) {
+		if (answer.equalsIgnoreCase(capital)) {
 			System.out.println("Correct!");
 		}
 		else {
