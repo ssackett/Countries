a389225440030ae104f30cd32b9d4ba6ca1c8984
Stephen Sackett <sesackett@gmail.com>
2017-07-27 11:22:23 -0400

    Add interractivity

--- Game.java
+++ Game.java
@@ -1,5 +1,15 @@
+import java.io.Console;
 class Game {
 	public static void main(String[] args){
-		System.out.println("Hello world");
+		System.out.println("Welcome to the Capitals game");
+		Console console = System.console();
+		String answer = console.readLine("What is the capital of Australia? ");
+		String capital = "Canberra";
+		if (answer.equals(capital)) {
+			System.out.println("Correct!");
+		}
+		else {
+			System.out.println("No, the answer is " + capital);
+		}
 	}
 }
